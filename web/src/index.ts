9ca2cfcc682560e2bffa845adeca5f37a8589bdf
ZainAmjad68 <zainamjad68@yahoo.com>
2021-12-20 11:54:32 +0500

    start working on Web Framework

---
+++ web/src/index.ts
@@ -0,0 +1,31 @@
+/*import { User } from "../models/User";
+
+let user1 = new User({ name: "Zain", age: 23 });
+
+console.log(user1.get("name"));
+user1.set({ name: "Amjad" });
+
+user1.on("change", () => {
+  console.log("change triggered");
+});
+
+user1.on("saved", () => {
+  console.log("Saved");
+});
+
+user1.trigger("change");
+user1.trigger("save");
+
+
+import axios from "axios";
+
+axios.post("http://localhost:3000/users", {name:"Zain", age:23});
+
+axios.get("http://localhost:3000/users/1");
+*/
+
+import { User } from "../models/User";
+
+let user1 = new User({ id:1 });
+
+user1.fetch();
