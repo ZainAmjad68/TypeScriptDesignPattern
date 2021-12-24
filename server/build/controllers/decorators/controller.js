b3e46dd62f423eee68076e5b45164e0e2571828e
ZainAmjad68 <zainamjad68@yahoo.com>
2021-12-24 16:03:39 +0500

    change tsconfig to target es5

--- server/build/controllers/decorators/controller.js
+++ server/build/controllers/decorators/controller.js
@@ -4,17 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.controller = exports.router = void 0;
-const express_1 = __importDefault(require("express"));
+var express_1 = __importDefault(require("express"));
 exports.router = express_1.default.Router();
 function controller(routePrefix) {
     return function (target) {
-        for (let key in target.prototype) {
-            const routeHandler = target.prototype[key];
-            const path = Reflect.getMetadata('path', target.prototype, key);
+        for (var key in target.prototype) {
+            var routeHandler = target.prototype[key];
+            var path = Reflect.getMetadata('path', target.prototype, key);
             console.log('target', target);
             console.log('path', path);
             if (path) {
-                exports.router.get(`${routePrefix}${path}`, routeHandler);
+                exports.router.get("".concat(routePrefix).concat(path), routeHandler);
             }
         }
     };
