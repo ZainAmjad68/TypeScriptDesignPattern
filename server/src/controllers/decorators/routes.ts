bae61e01b5b7c81c9fd6c08b0a32ee6280fb6a30
ZainAmjad68 <zainamjad68@yahoo.com>
2021-12-27 10:29:00 +0500

    completed the Express integration with TS

--- server/src/controllers/decorators/routes.ts
+++ server/src/controllers/decorators/routes.ts
@@ -1,7 +1,23 @@
+import { RequestHandler } from 'express';
 import 'reflect-metadata';
+import { MetadataKeys } from './MetadataKeys';
+import { Methods } from './Methods';
 
-export function get(path: string) {
-    return function(target: any, key: string, desc: PropertyDescriptor) {
-        Reflect.defineMetadata('path', path, target, key);
+interface RouteHandlerDescriptor extends PropertyDescriptor {   // makes sure that any method that doesn't have a req, res arguments can't be binded to out get() decorator
+    value?: RequestHandler
+}
+
+function RouteBinder(method:string) {
+    return function(path: string) {
+        return function(target: any, key: string, desc: RouteHandlerDescriptor) {
+            Reflect.defineMetadata(MetadataKeys.path, path, target, key);
+            Reflect.defineMetadata(MetadataKeys.method, method, target, key);
+        }
     }
 }
+
+export const get = RouteBinder(Methods.get);
+export const post = RouteBinder(Methods.post);
+export const del = RouteBinder(Methods.del);
+export const put = RouteBinder(Methods.put);
+export const patch = RouteBinder(Methods.patch);
